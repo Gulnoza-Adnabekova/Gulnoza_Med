1f3fb049a0f7c875cdd89be5a93ff02334789aa8
Behruz22 <behruztuxtayev8@gmail.com>
2025-04-16 17:42:29 +0500

    db changes _context

--- Gulnoza_Med/Controllers/HomeController.cs
+++ Gulnoza_Med/Controllers/HomeController.cs
@@ -5,44 +5,52 @@ namespace Gulnoza_Med.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly AppDbContext db;
+        private readonly AppDbContext _context;
+
         public HomeController(AppDbContext db)
         {
-            this.db = db;
+            this._context = db;
         }
+
         public IActionResult HomePage()
         {
             var results = new Lists
             {
-                Doctors = db.Doctors.ToList(),
-                Fields = db.Fields.ToList()
+                Doctors = _context.Doctors.ToList(),
+                Fields = _context.Fields.ToList()
             };
             return View(results);
         }
+
         public IActionResult FieldsPage()
         {
-            var result = db.Fields.ToList();
+            var result = _context.Fields.ToList();
             return View(result);
         }
+
         public IActionResult DoctorsPage()
         {
-            var result = db.Doctors.ToList();
+            var result = _context.Doctors.ToList();
             return View(result);
         }
+
         public IActionResult TestimonialPage()
         {
             return View();
         }
+
         public IActionResult ContactUsPage()
         {
             return View();
         }
+
         public IActionResult DoctorDetailsPage(int id)
         {
-            var result = db.Doctors.ToList().FirstOrDefault(x => x.Id == id);
+            var result = _context.Doctors.ToList().FirstOrDefault(x => x.Id == id);
             return View(result);
         }
     }
+
     public class Lists
     {
         public List<Doctors> Doctors { get; set; }
