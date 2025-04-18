82259ffd33d5cfe6e137676bd0a48e758169a4d0
Behruz22 <behruztuxtayev8@gmail.com>
2025-04-18 18:08:00 +0500

    change

--- Gulnoza_Med/Controllers/HomeController.cs
+++ Gulnoza_Med/Controllers/HomeController.cs
@@ -1,70 +1,76 @@
 ﻿using Gulnoza_Med.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Gulnoza_Med.Controllers
+namespace Gulnoza_Med.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly AppDbContext _context;
+
+    public HomeController(AppDbContext context)
     {
-        private readonly AppDbContext db;
-        public HomeController(AppDbContext db)
-        {
-            this.db = db;
-        }
-        
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public IActionResult Search(string query)
-        {
-
-            if (string.IsNullOrWhiteSpace(query))
-                return BadRequest("Qidiruv so‘zi kiritilmagan!");
-
-            var results = db.Doctors
-                .Where(p => p.Name.ToLower().Contains(query.ToLower()))
-                .ToList();
-
-            return Json(results);
-        }
-        public IActionResult HomePage()
-        {
-            var results = new Lists
-            {
-                Doctors = db.Doctors.ToList(),
-                Fields = db.Fields.ToList()
-            };
-            return View(results);
-        }
-        public IActionResult FieldsPage()
-        {
-            var result = db.Fields.ToList();
-            return View(result);
-        }
-        public IActionResult DoctorsPage()
-        {
-            var result = db.Doctors.ToList();
-            return View(result);
-        }
-        public IActionResult TestimonialPage()
-        {
-            return View();
-        }
-        public IActionResult ContactUsPage()
-        {
-            return View();
-        }
-        public IActionResult DoctorDetailsPage(int id)
-        {
-            var result = db.Doctors.ToList().FirstOrDefault(x => x.Id == id);
-            return View(result);
-        }
+        this._context = context;
     }
-    public class Lists
+
+    public IActionResult Index()
     {
-        public List<Doctors> Doctors { get; set; }
-        public List<Fields> Fields { get; set; }
+        return View();
     }
+
+    [HttpGet]
+    public IActionResult Search(string query)
+    {
+        if (string.IsNullOrWhiteSpace(query))
+            return BadRequest("Qidiruv so‘zi kiritilmagan!");
+
+        var results = _context.Doctors
+            .Where(p => p.Name.ToLower().Contains(query.ToLower()))
+            .ToList();
+
+        return Json(results);
+    }
+
+    public IActionResult HomePage()
+    {
+        var results = new Lists
+        {
+            Doctors = _context.Doctors.ToList(),
+            Fields = _context.Fields.ToList()
+        };
+        return View(results);
+    }
+
+    public IActionResult FieldsPage()
+    {
+        var result = _context.Fields.ToList();
+        return View(result);
+    }
+
+    public IActionResult DoctorsPage()
+    {
+        var result = _context.Doctors.ToList();
+        return View(result);
+    }
+
+    public IActionResult TestimonialPage()
+    {
+        return View();
+    }
+
+    public IActionResult ContactUsPage()
+    {
+        return View();
+    }
+
+    public IActionResult DoctorDetailsPage(int id)
+    {
+        var result = _context.Doctors.ToList().FirstOrDefault(x => x.Id == id);
+        return View(result);
+    }
+}
+
+public class Lists
+{
+    public List<Doctors> Doctors { get; set; }
+    public List<Fields> Fields { get; set; }
 }
