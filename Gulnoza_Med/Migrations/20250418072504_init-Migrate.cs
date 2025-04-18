d63ca5db66b4145bf4e705fe8e19fd3e251ae14e
Behruz22 <behruztuxtayev8@gmail.com>
2025-04-18 14:08:46 +0500

    mm,mm

---
+++ Gulnoza_Med/Migrations/20250418072504_init-Migrate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gulnoza_Med.Migrations
+{
+    /// <inheritdoc />
+    public partial class initMigrate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
